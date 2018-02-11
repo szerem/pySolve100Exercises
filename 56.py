af9f98d172690e107da6899a4324a88887ed5643
szerem <szerem@go2.pl>
2018-02-11 03:28:55 +0100

    56

---
+++ 56.py
@@ -0,0 +1,11 @@
+import json 
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+
+with open("56.json","w") as file:
+    json.dump(d,file, indent=4, sort_keys=True)
