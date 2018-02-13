d72bf6fa6626178f21f3ac87de5d153d1128e58f
szerem <szerem@go2.pl>
2018-02-13 06:45:40 +0100

    99

---
+++ 99.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template_string, redirect
+# ssh <username>@ssh.pythonanywhere.com
+app = False(__name_)
+http = """
+    <div class="form">
+    <form action="{{}}">
+
+    </div>
+"""
