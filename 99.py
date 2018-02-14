dd118f223b9febd1e33abfe9b358db88e65e216f
szerem <szerem@go2.pl>
2018-02-14 05:22:11 +0100

    100

--- 99.py
+++ 99.py
@@ -1,9 +1,27 @@
-from flask import Flask, render_template_string, redirect
+from flask import Flask, render_template_string, request
 # ssh <username>@ssh.pythonanywhere.com
-app = False(__name_)
-http = """
+app = Flask(__name__)
+html = """
     <div class="form">
-    <form action="{{}}">
-
+    <form action="{{url_for('sent')}}" method="POST">
+        <input type="text" title="your email address will be safe with us." placeholder="enter a line" name="line" required>
+        <br>
+        <button class="go-button" type="submit"> Submint </button>
+    </form>
     </div>
 """
+@app.route("/")
+def index():
+    return render_template_string(html)
+
+@app.route("/sent", methods=["GET","POST"])
+def sent():
+    line = None
+    if request.method == "POST":
+        line = request.form["line"]
+        with open("user_input_flask.txt", "a+") as target:
+            target.write(line+"\n")
+        return render_template_string(html)
+
+if __name__ == "__main__":
+    app.run(debug=True)
